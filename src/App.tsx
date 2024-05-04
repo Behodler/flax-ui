782ffd2e10b84016f88682f875fa12c8f47d0729
gititgoro <admin@behodler.io>
2024-05-04 07:31:00 +0200

    Grid beginnings

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <div style={ {overflowX: 'hidden'}}>
         <MinterPage />
       </div>
     </ThemeProvider>
--- src/components/MinterPage.tsx
+++ src/components/MinterPage.tsx
@@ -4,14 +4,23 @@ import { Grid, Container, Box } from '@mui/material';
 const MinterPage = () => {
   return (
     <Container maxWidth={false} disableGutters>
-      <Grid container spacing={0} sx={{ backgroundColor: '#0D131A', width: '100vw', height: '100vh' }}>  // Ensure full height
+      <Grid container spacing={0} sx={{ backgroundColor: '#0D131A', width: '100vw', minHeight: '100vh', boxSizing: 'border-box' }}>
         <Grid item xs={false} sm={1} md={true} style={{ flexGrow: 1 }} />
         <Grid item xs={12} sm={10} md={false} style={{ maxWidth: 1100, flexBasis: 1100 }}>
-          <Box sx={{ height: '100vh', width: '100%', color: 'white', padding: 4, backgroundColor: '#0D131A' }}>
-            {/* Main content goes here */}
-            <Box sx={{ width: 300, height: 200, backgroundColor: '#1D2833', margin: 'auto', textAlign: 'center', padding: 2 }}>
-              Small Floating Item
-            </Box>
+          <Box id="box" sx={{ minHeight: '100%', width: '100%', color: 'white', padding: 4, backgroundColor: '#0D131A', boxSizing: 'border-box' }}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="stretch"
+            >
+              <Grid item style={{ height: '172px', border: '1px solid orange' }}>
+                {/* Content for the first row (fixed height) */}
+              </Grid>
+              <Grid item style={{ minHeight: '600px', border: '1px solid orange' }}>
+                <Box sx={{ height: '800px', width: '600px', color: 'white', padding: 4, backgroundColor: 'white', boxSizing: 'border-box' }} />
+              </Grid>
+            </Grid>
           </Box>
         </Grid>
         <Grid item xs={false} sm={1} md={true} style={{ flexGrow: 1 }} />
